2c22a0d0b4e11ba362577147e029c3901c971f4c
david-commit <david.ondiege@students.moringaschool.com>
2022-10-06 23:32:59 +0300

    Trainer details succesful fetch

--- src/components/Trainers.js
+++ src/components/Trainers.js
@@ -4,7 +4,28 @@ import { useEffect, useState } from 'react';
 function Trainers() {
   const [trainers, setTrainers] = useState([]);
 
-  
+  useEffect(() => {
+    fetch('http://localhost:3000/trainers')
+      .then((response) => response.json())
+      .then((data) => setTrainers(data));
+  }, []);
+  console.log(trainers);
+
+  const mappedTrainers = trainers.map((trainer) => {
+    return (
+      <div className='trainer-card' key={trainer.id}>
+        <img src={trainer.image} alt='Trainer Image' />
+        <div className='trainer-details'>
+          <h3>{trainer.name}</h3>
+          <p>{trainer.title}</p>
+          <a href={trainer.facebook} class='fab fa-facebook-f'></a>
+          <a href={trainer.twitter} class='fab fa-twitter'></a>
+          <a href={trainer.linkedin} class='fab fa-linkedin'></a>
+          <a href={trainer.pintrest} class='fab fa-pinterest'></a>
+        </div>
+      </div>
+    );
+  });
   return (
     <>
       <h1 className='trainers-heading'>Meet the Team</h1>
