2d8a1d3698a557e9c9ed78b9693b26cf9fe24729
david-commit <david.ondiege@students.moringaschool.com>
2022-10-06 23:42:43 +0300

    Contact Component: Added contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className='contact'>
+      <h1>Contact Us</h1>
+      <h3>Address</h3>
+      <p>
+        Sameer Industrial Park - Mombasa Road <br />
+        P.O. Box 27719 – 00506 GPO Nairobi, Kenya{' '}
+      </p>
+      <h3>Phone Mobile</h3>
+      <p>
+        (+254) 714 392898 <br />
+        (+254) 733 483396{' '}
+      </p>
+      <h3>Email</h3>
+      <p>
+        administration@vermont-design.com <br />
+        sales@vermont-design.com
+      </p>
+
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
