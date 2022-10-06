75f4aa8f3c74018d5d3d31f9a47d03f3eab3221b
david-commit <david.ondiege@students.moringaschool.com>
2022-10-06 23:31:57 +0300

    Trainer component creation

--- src/components/Trainers.js
+++ src/components/Trainers.js
@@ -1,40 +1,9 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React from 'react'
 
 function Trainers() {
-  const [trainers, setTrainers] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/trainers')
-      .then((response) => response.json())
-      .then((data) => setTrainers(data));
-  }, []);
-  console.log(trainers);
-
-  const mappedTrainers = trainers.map((trainer) => {
-    return (
-      <div className='trainer-card' key={trainer.id}>
-        <img src={trainer.image} alt='Trainer Image' />
-        <div className='trainer-details'>
-          <h3>{trainer.name}</h3>
-          <p>{trainer.title}</p>
-          <a href={trainer.facebook} class='fab fa-facebook-f'></a>
-          <a href={trainer.twitter} class='fab fa-twitter'></a>
-          <a href={trainer.linkedin} class='fab fa-linkedin'></a>
-          <a href={trainer.pintrest} class='fab fa-pinterest'></a>
-        </div>
-      </div>
-    );
-  });
   return (
-    <>
-      <h1 className='trainers-heading'>Meet the Team</h1>
-      <div className='trainers'>{mappedTrainers}</div>
-      <div className='join-us'>
-        <button type='button'>JOIN THE TEAM</button>
-      </div>
-    </>
-  );
+    <div>Trainers</div>
+  )
 }
 
-export default Trainers;
+export default Trainers
