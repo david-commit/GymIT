da0fd9e4bfc7b0400b1f4cfbff8967ef170b6b7c
david-commit <david.ondiege@students.moringaschool.com>
2022-10-06 23:32:40 +0300

    Trainer state cofiguration

--- src/components/Trainers.js
+++ src/components/Trainers.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 function Trainers() {
+  const [trainers, setTrainers] = useState([]);
+
+  
   return (
-    <div>Trainers</div>
-  )
+    <>
+      <h1 className='trainers-heading'>Meet the Team</h1>
+      <div className='trainers'>{mappedTrainers}</div>
+      <div className='join-us'>
+        <button type='button'>JOIN THE TEAM</button>
+      </div>
+    </>
+  );
 }
 
-export default Trainers
+export default Trainers;
